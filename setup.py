abd13796840371c78f3b6a619b742800c536fe4a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:29:17 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-MorphologicalAnalysis',
     version='1.0.27',
     packages=['MorphologicalAnalysis'],
-    url='https://github.com/olcaytaner/TurkishMorphologicalAnalysis-Py',
+    url='https://github.com/StarlangSoftware/TurkishMorphologicalAnalysis-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Morphological Analysis',
     install_requires=['NlpToolkit-Dictionary', 'NlpToolkit-Corpus', 'NlpToolkit-DataStructure']
 )
