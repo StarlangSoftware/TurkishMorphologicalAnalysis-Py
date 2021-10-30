c0bc8c93b573961744d9e47cd7af1d2c04748314
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:49:17 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-MorphologicalAnalysis',
-    version='1.0.34',
+    version='1.0.35',
     packages=['MorphologicalAnalysis', 'DisambiguationCorpus'],
     url='https://github.com/StarlangSoftware/TurkishMorphologicalAnalysis-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Morphological Analysis',
-    install_requires=['NlpToolkit-Dictionary', 'NlpToolkit-Corpus', 'NlpToolkit-DataStructure']
+    install_requires=['NlpToolkit-Dictionary', 'NlpToolkit-Corpus', 'NlpToolkit-DataStructure'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
