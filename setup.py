a0314a52ed32fabf2bed711d7935dad3047a2bff
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-09-14 09:27:49 +0300

    Updated longestRootExceptions.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name='NlpToolkit-MorphologicalAnalysis',
-    version='1.0.41',
+    version='1.0.42',
     packages=['MorphologicalAnalysis', 'MorphologicalAnalysis.data', 'DisambiguationCorpus'],
     package_data={'MorphologicalAnalysis.data': ['*.xml']},
     url='https://github.com/StarlangSoftware/TurkishMorphologicalAnalysis-Py',
