9596b9061d6f7dee9934fd9bade8c8a2f2550c6f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-10-28 13:09:17 +0300

    Removed formation_to_check from attribute list of Transition class.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name='NlpToolkit-MorphologicalAnalysis',
-    version='1.0.44',
+    version='1.0.45',
     packages=['MorphologicalAnalysis', 'MorphologicalAnalysis.data', 'DisambiguationCorpus'],
     package_data={'MorphologicalAnalysis.data': ['*.xml']},
     url='https://github.com/StarlangSoftware/TurkishMorphologicalAnalysis-Py',
